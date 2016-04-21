1195aa5519e8b5d48c2226aefc9e31b526c25499
Jack Anteby <Janteby1@gmail.com>
2016-04-21 10:25:55 -0400

    starting to build out django view

---
+++ project/tables/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from tables import views #gets all our view functions
+
+urlpatterns = [
+    url(r'^$', views.Index.as_view(), name='index'),
+
+    # url(r'^register$', views.Register.as_view(), name='register'),
+    # url(r'^login$', views.Login.as_view(), name='login'),
+    # url(r'^logout$', views.Logout.as_view(), name='logout'),
+]
+
--- project/tables/views.py
+++ project/tables/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
+# from django.db.models import Q
+
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+
+import json
+# import twython import Twython
+
+# from .forms import UserForm, LoginForm, CreateForm
+from .models import Neighborhood, Ages, Economic, SchoolEducation, Building, Demographic, UnitValue, UnitDescription
 
 # Create your views here.
+class Index(View):
+    def get(self, request):
+
+        return render(request, "form.html")
