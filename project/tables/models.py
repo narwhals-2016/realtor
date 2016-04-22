f4a00da88c82d831a9277d49e545aa745bbe1559
Jack Anteby <Janteby1@gmail.com>
2016-04-22 12:14:49 -0400

    working render of all forms, register and login

--- project/tables/models.py
+++ project/tables/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Neighborhood(models.Model):
@@ -9,6 +10,20 @@ class Neighborhood(models.Model):
     def to_json(self):
         return {
             "name": self.name,
+        }
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    # search = models.CharField(max_length=3000, default=None, Null=True)
+    '''
+    we can add aditional attributes but Included in the django user model are these attributes:
+    Username, Password, Email address, firstname, lastname
+    '''
+
+    def to_json(self):
+        return {
+            "email": user.email,
+            "username": user.username,
         }
 
 class Ages(models.Model):
