128c0f60e8b69ce10933189e3b7f79d9e541bbba
Jack Anteby <Janteby1@gmail.com>
2016-05-03 22:24:51 -0400

    added view map button

--- project/tables/static/js/results.js
+++ project/tables/static/js/results.js
@@ -1,5 +1,5 @@
-function init_map(map_div,latitude, longitude) {
-	console.log(latitude, longitude);
+function init_street_view(map_div,latitude, longitude) {
+	// console.log(latitude, longitude);
 	var var_location = new google.maps.LatLng(latitude,longitude);
 
 	var var_mapoptions = {
@@ -7,7 +7,6 @@ function init_map(map_div,latitude, longitude) {
 		zoom: 14,
 	};
 	var var_map = new google.maps.Map(map_div, var_mapoptions);
-
 
 	var var_marker = new google.maps.Marker({
 		position: var_location,
@@ -28,9 +27,39 @@ function init_map(map_div,latitude, longitude) {
 
 }
 
+function init_map(map_div,latitude, longitude) {
+	// console.log(latitude, longitude);
+	var var_location = new google.maps.LatLng(latitude,longitude);
+
+	var var_mapoptions = {
+		center: var_location,
+		zoom: 14,
+	};
+	var var_map = new google.maps.Map(map_div, var_mapoptions);
+
+	var var_marker = new google.maps.Marker({
+		position: var_location,
+		map: var_map,
+	});
+
+  	// var panorama = new google.maps.StreetViewPanorama(
+   //    map_div, {
+   //      position: var_location,
+   //      pov: {
+   //        heading: 34,
+   //        pitch: 10
+   //      }
+    // });
+  	// var_map.setStreetView(panorama);
+	var_marker.setMap(var_map); 
+
+}
+
+
 $(document).ready(function(){
 
-	$(".bold_hover").on("click", function(event){
+	// street view 
+	$(".street_view").on("click", function(event){
 		event.preventDefault();
 		// console.log('inside bold_hover clicked')
         var $map = $(this).parent();
@@ -38,10 +67,27 @@ $(document).ready(function(){
         var $image_card = cards.first();
 
 		$map.css('height',$image_card.css('height'));
-		// $('#map-div').css('')
+		var latitude = $map.data('lat');
+		var longitude = $map.data('long');
+		init_street_view($map[0], latitude, longitude);
+
+	});
+
+	// map view
+	$(".map_view").on("click", function(event){
+		event.preventDefault();
+		// console.log('inside bold_hover clicked')
+        var $map = $(this).parent();
+        var cards = $map.parents('.row').children();
+        var $image_card = cards.first();
+
+		$map.css('height',$image_card.css('height'));
 		var latitude = $map.data('lat');
 		var longitude = $map.data('long');
 		init_map($map[0], latitude, longitude);
 
 	});
+
 });
+
+
